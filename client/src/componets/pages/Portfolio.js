ecf56e1d0f071b1bb77f0f2426dae440b09581a3
Aaron Ray <aaray619@gmail.com>
2020-11-24 09:39:48 -0600

    set up intial portfolio data and connected to the portfolio page

--- client/src/componets/pages/Portfolio.js
+++ client/src/componets/pages/Portfolio.js
@@ -1,10 +1,20 @@
 import React from "react";
+import projects from "../../projects/projects.json"
 
 function Portfolio(){
+   
     return(
         <>
         <div>
+            {console.log(projects)}
             <h3>Test Portfolio</h3>
+            <ul>
+                {projects.map(project => {
+                    return(
+                    <li>{project.name}</li>
+                    )
+                })}
+            </ul>
         </div>
         </>
     )
